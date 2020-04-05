1e82ff7cda6481c77813514a386f04c4e5e4bb5a
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-05 23:58:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,8 +71,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 holiday_hash.each do |season, holiday|
   data.each do |holiday, supply|
-    
-  
+
+
   binding.pry
 end 
 end
