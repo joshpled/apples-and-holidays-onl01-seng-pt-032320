06bd90af341e58ce6782e0f3ea0a9cb70629c386
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-05 21:45:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,6 +61,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+holiday_hash.each do |season, holiday|
+  puts season.capitalize!
+  puts holiday.capitalize!
+end 
 
 end
 
