ec5479b747d27be99dc9250b031ea06fbc369fda
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-05 21:08:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,8 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holiday|
     if season == :winter
-      holiday << supply
-      binding.pry
+      puts season
     end
   end
 
