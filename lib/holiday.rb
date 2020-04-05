1210fa7abfcc04287adf7eab05680b4a4c488a01
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-05 23:00:50 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,11 +67,9 @@ def all_supplies_in_holidays(holiday_hash)
         holidays.to_s.split("_")
         puts "#{holidays.capitalize}: #{supplies.join(", ")}"
       end
-    end 
     end
-end
-
-end
+  end
+end 
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
